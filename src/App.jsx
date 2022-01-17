7c87f9fab7ed8722a8c5fae73cc8a83edc7cf331
jgarciac08 <jgarciac08@educantabria.es>
2022-01-17 13:58:40 +0100

    sfcasfasf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Home } from './components/Home';
-import { PaginaListas } from './components/PaginaListas';
-import { Perfil } from './components/Perfil';
 import Header from './components/Header';
 import { MenuItems } from './data/MenuItems';
 import BootstrapHeader from './components/BootstrapHeader';
@@ -11,7 +8,7 @@ import BootstrapHeader from './components/BootstrapHeader';
 export function App() {
   return (
     <Router>
-      <Header />
+      {/*<Header />*/}
       <BootstrapHeader/>
       {MenuItems.map((item) => {
         return (
